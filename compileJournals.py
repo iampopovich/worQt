a3cd7768a915bbe44c0268363f74dd7bf0d3d956
Alex <enterport6@gmail.com>
2018-08-08 20:15:38 +0300

    pre compiled non stable first tested version

--- compileJournals.py
+++ compileJournals.py
@@ -6,6 +6,7 @@ import time as tt
 import datetime as dt
 import re
 import os
+from os import path
 import sys
 '''
 def findFiles(cwd):
@@ -13,58 +14,69 @@ def createJournal(cwd):
 def compileFile(jList,config,journal,cwd):
 def readWriteConfig(conf,mode,dict = None):
 '''
+def whatOSRun(path):
+	if os.name.endswith('nt'):
+		return '%s\\' %path
+	else:
+		return '%s/' %path
 def getHeaders(key):
 	names = {'ДД':['Директивный документ','Этап контроля','Обозначение ДД',
 					'Размещение ДД в структуре папок ОКБ','Процедура выпуска',
 					'Оформление ДД','Соответствие ДД модели данных',
 					'Модуль создания ДД','Применяемость','Указание о внедрении/заделе',
 					'Согласовано','Примечание','Дата','Проверку выполнил'],
-			'ДТЭ':['Обозначение КД/Ревизия-Наименование',
+			'ДТЭ':['Обозначение КД\\Ревизия-Наименование',
 					'Тип объекта','Владелец','Подразделение','Обозначение ДД',
-					'Этап контроля','Обозначение /Процедура выпуска',
-					'Размещение в структуре папок ОКБ\nВходимость в головную сборку/проект',
-					'Соответствие модели данных/Время сохранения',
-					'Оформление/Состав ЭМ','Ограничения/WAVE-связи','Масса/Материал/Атрибуты',
-					'Геометрия/Допуски','Слои/Ссылочные наборы','Анализ зазоров','Согласовано'
+					'Этап контроля','Обозначение \\ Процедура выпуска',
+					'Размещение в структуре папок ОКБ\nВходимость в головную сборку\\проект',
+					'Соответствие модели данных\\Время сохранения',
+					'Оформление\\Состав ЭМ','Ограничения\\WAVE-связи','Масса\\Материал\\Атрибуты',
+					'Геометрия\\Допуски','Слои\\Ссылочные наборы','Анализ зазоров','Согласовано'
 					'Документ на отклонение от требований НД','Дата','Проверку выполнил']}
 	return names.get(key)
 
+
 def findFiles(cwd):
 	currentYear = dt.datetime.now().year
-	xlFiles = [f for f in os.listdir(cwd) if isfile(os.join(cwd, f))]
-	xlFiles = [lambda x: re.search(r'.*DTE.*%s.*xlsm' %currentYear, xlFiles).group(0)]
-	for xlItem in xlFiles:
-		if re.search(r'.*Сводный_журнал_ДТЭ.*%s.*xlsm' %currentYear, xlItem) == None:
-			createJournal(cwd)
+	xlFiles = [path.join(cwd, f) for f in os.listdir(cwd) if path.isfile(path.join(cwd, f))] #сеим директории, оставляем файлы
+	try:	#этих файлов может и не быть , на всякий случай расставь экспы
+		cJournalName = '%s/Сводный_журнал_ДТЭ_%s.xlsm' %(cwd,currentYear)
+		if cJournalName in xlFiles:
+			xlFiles.pop(xlFiles.index(cJournalName))
 		else:
-			cJournal = xlItem
-			xlFiles.pop(xlFiles.index(xlItem))
-	return xlFiles,cJournal
+			createJournal(cJournalName)
+		for xlItem in xlFiles:
+			if re.search(r'.*\sДТЭ\s.*%s.*xlsm' %currentYear, xlItem) is None:
+				xlFiles.pop(xlFiles.index(xlItem))	
+		return xlFiles,cJournalName
+	except Exception as ex:
+		print('findFiles failed with: %s' %ex)
 
-def createJournal(cwd):
+def createJournal(name):
 	wb = openpyxl.Workbook()
-	for name in ['ДД','ДТЭ']:
-		wb.create_sheet(name)
-		activeSheet = wb.get_sheet_by_name(name)
-		activeSheet.append(getHeaders(name))
-	currentYear = dt.datetime.now().year
-	wb.save('%s\\Сводный_журнал_ДТЭ_%s.xlsm' %(cwd,currentYear))
+	for sheetName in ['ДД','ДТЭ']:
+		wb.create_sheet(sheetName)
+		activeSheet = wb.get_sheet_by_name(sheetName)
+		activeSheet.append(getHeaders(sheetName))
+	wb.save('%s' %(name))
 	return None
 	
 def compileFile(jList,journal,cwd): #by cells
 	wbToCopy = openpyxl.load_workbook(journal)
 	#####clear compiled book
-	for sheet in wbFromCopy.sheetnames:
-		for row in sheet.iter_rows(row_offset=1, min_row = 1,max_row = sheet.max_row): #check specs for iter_rows
+	for sheet in wbToCopy.sheetnames:
+		cSheet = wbToCopy.get_sheet_by_name(sheet)
+		for row in cSheet.iter_rows(): #check specs for iter_rows
 			for cell in row:
-				sheetToCopy.cell(row = row ,column = cell.col_idx, value ='')
+				cSheet.cell(row = cSheet._current_row, column = cell.col_idx, value ='')
+	print('HHHHHHHHHHHHHHHHHHUUUUUUUUURRRRRRRRRRRAAAAAAAAAAAHHHHHHHHHHH')
 	#############
 	for jItem in jList: 
 		wbFromCopy = openpyxl.load_workbook(jItem,read_only=True)
 		for sheetFromCopy in wbFromCopy.sheetnames:
 			sheetName = sheetFromCopy.title.split('_')[0]  #example : DTE_NAME
 			sheetToCopy = wbToCopy.get_sheet_by_name(sheetName.title) #copy cells to the same sheet in new workbook
-			for row in sheet.iter_rows(row_offset=1, min_row = 1,max_row = sheetFromCopy.max_row): #check specs for iter_rows
+			for row in sheet.iter_rows(): #check specs for iter_rows
 				lastRow = sheetToCopy.max_row
 				for cell in row:
 					newCell = sheetToCopy.cell(row = lastRow + 1 ,column = cell.col_idx, value = cell.value)
@@ -80,7 +92,7 @@ def compileFile(jList,journal,cwd): #by cells
 	
 def main():
 	tStart = dt.datetime.now().hour
-	workDir = os.getcwd()
+	workDir = whatOSRun(os.getcwd())
 	stopSwitch = False
 	journalList,journalFile = findFiles(workDir)
 	while not(stopSwitch):
