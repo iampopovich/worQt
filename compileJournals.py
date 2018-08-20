173bcb9aa330ceb9c4eb49e23d5d31317b41bdff
Alexander <iampopovich@gmail.com>
2018-08-20 19:38:15 +0300

    added styles
    still 0.9 . in testing

--- compileJournals.py
+++ compileJournals.py
@@ -1,12 +1,12 @@
 #!/usr/bin/pythom
 #-*-coding: utf-8-*-
-import math
+#import math
 import openpyxl
 import time as tt
 import datetime as dt
 import re
 import os
-from os import path
+#from os import path
 import sys
 import lib_headers as hh
 '''
@@ -18,35 +18,49 @@ def readWriteConfig(conf,mode,dict = None):
 def whatOSRun(path):
 	if os.name.endswith('nt'):
 		return '%s\\' %path
-	else:
-		return '%s/' %path
+	else: return '%s/' %path
 
 def findFiles(cwd):
 	currentYear = dt.datetime.now().year
-	files = [path.join(cwd, f) for f in os.listdir(cwd) if path.isfile(path.join(cwd, f))] #сеим директории, оставляем файлы
+	files = [os.path.join(cwd, f) for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
 	try:	#этих файлов может и не быть , на всякий случай расставь экспы
 		cJournalName = '%sСводный_журнал_ДТЭ_%s.xlsx' %(cwd,currentYear)
-		xlFiles = [item for item in files if '.xlsm' in item]
-		if cJournalName in xlFiles:
-			xlFiles.pop(xlFiles.index(cJournalName))
-		else:
-			createJournal(cJournalName)
-		for xlItem in xlFiles:
-			if re.search(r'.*\sДТЭ\s.*%s.*xlsm' %currentYear, xlItem) is None:
-				xlFiles.pop(xlFiles.index(xlItem))	
+		if cJournalName in files: files.pop(files.index(cJournalName))
+		else: createJournal(cJournalName)
+		xlFiles = [item for item in files if not(re.search(r'.*\sДТЭ\s.*%s.*xlsm' %currentYear, item) is None)]
 		return xlFiles,cJournalName
 	except Exception as ex:
 		print('findFiles failed with: %s' %ex)
+		return None, None
 
 def createJournal(name):
-	wb = openpyxl.Workbook()
-	wb.remove(wb.active)
-	for sheetName in ['ДД','ДТЭ']:
-		wb.create_sheet(sheetName)
-		activeSheet = wb[sheetName]
-		activeSheet.append(hh.getHeaders(sheetName))
-	wb.save('%s' %(name))
-	return None
+	try:
+		wb = openpyxl.Workbook()
+		wb.remove(wb.active)
+		for sheetName in ['ДД','ДТЭ']:
+			wb.create_sheet(sheetName)
+			activeSheet = wb[sheetName]
+			activeSheet.append(hh.getHeaders(sheetName))
+			#activeSheet.auto_filter.ref = 
+			for cell in list(activeSheet.rows)[0]:
+				if sheetName == 'ДД':
+					cell.style = 'Input'
+					border = openpyxl.styles.Border(left=openpyxl.styles.Side(style='thin'),
+                     						right=openpyxl.styles.Side(style='thin'),
+                     						top=openpyxl.styles.Side(style='thin'),
+                     						bottom=openpyxl.styles.Side(style='thin'))
+					cell.border = border
+				else:
+					cell.style = 'Accent5'
+					border = openpyxl.styles.Border(left=openpyxl.styles.Side(style='thin'),
+                     						right=openpyxl.styles.Side(style='thin'),
+                     						top=openpyxl.styles.Side(style='thin'),
+                     						bottom=openpyxl.styles.Side(style='thin'))
+					cell.border = border
+		wb.save('%s' %(name))
+		return None
+	except Exception as ex:
+		return ('createJournal failed with: %s' %ex)
 	
 def clearJournal(journal):
 	wbToCopy = openpyxl.load_workbook(journal)
@@ -58,25 +72,34 @@ def clearJournal(journal):
 	wbToCopy.save(journal)
 
 def compileFile(jList,journal): #by cells
-	wbToCopy = openpyxl.load_workbook(journal)
+	try:
+		wbToCopy = openpyxl.load_workbook(journal)
+	except Exception as ex:
+		return 'CompileJournal failed with %s' %ex
 	tempLastRow = {'ДД':2,'ДТЭ':2} 
 	for jItem in jList: 
 		wbFromCopy = openpyxl.load_workbook(jItem,read_only=True)
 		for sheetFromCopy in wbFromCopy.sheetnames:
 			try:
-				sheetToCopy = wbToCopy[sheetFromCopy.split('_')[0]] #copy cells to the same sheet in new workbook
+				subname = sheetFromCopy.split('_')[0]
+				sheetToCopy = wbToCopy[subname] #copy cells to the same sheet in new workbook
 				sheet = wbFromCopy[sheetFromCopy]
 			except:	continue
 			for row in sheet.iter_rows(min_row = 2):
 				isEmptyRow = True
-				lastRow = tempLastRow[sheetFromCopy.split('_')[0]]
+				lastRow = tempLastRow[subname]
 				for iCell,cell in enumerate(row):
 					if (cell.value is None):
-						if iCell in range(2,13):
-							newCell = sheetToCopy.cell(row = lastRow,column = iCell+1, value = ' ')
-							newCell.border = cell.border
-							bgCLR = openpyxl.styles.colors.Color(rgb='00c6efce')
-							newCell.fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=bgCLR)
+						newCell = sheetToCopy.cell(row = lastRow,column = iCell+1, value = ' ')
+						newCell.style = 'Good'
+						border = openpyxl.styles.Border(left=openpyxl.styles.Side(style='thin'),
+                     						right=openpyxl.styles.Side(style='thin'),
+                     						top=openpyxl.styles.Side(style='thin'),
+                     						bottom=openpyxl.styles.Side(style='thin'))
+						newCell.border = border
+						#newCell.border = cell.border
+						#bgCLR = openpyxl.styles.colors.Color(rgb='00c6efce')
+						#newCell.fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=bgCLR)
 					else:
 						isEmptyRow = False 
 						newCell = sheetToCopy.cell(row = lastRow,column = cell.column, value = cell.value)
@@ -84,18 +107,17 @@ def compileFile(jList,journal): #by cells
 						newCell.border = cell.border
 						newCell.fill = cell.fill
 						newCell.number_format = cell.number_format
-						newCell.protection = cell.protection
 						newCell.alignment = cell.alignment
 				if isEmptyRow:	continue
-				else: tempLastRow[sheetFromCopy.split('_')[0]]+=1
+				else: tempLastRow[subname]+=1
 		wbFromCopy.close()
+		del wbFromCopy, sheetFromCopy 
 		wbToCopy.save(journal)
-
 	wbToCopy.close()
 	return None # ¯\_(ツ)_/¯
 	
 def main():
-	tStart = dt.datetime.now().hour
+	tStart = dt.datetime.now()#.hour
 	workDir = whatOSRun(os.getcwd()) #определяем параметры ввода пути до файла
 	stopSwitch = False
 	journalList,journalFile = findFiles(workDir)
@@ -104,10 +126,11 @@ def main():
 		#clearJournal(journalFile)
 		compileFile(journalList,journalFile)
 		print('waiting for 10 seconds')
-		tt.sleep(10)
+		print(dt.datetime.now() - tStart)
 		sys.exit(0) # пока выходит после сборки 
 		#wait for 15 minutes and repeat cycle
 		#stopSwitch = ((dt.datetime.now().hour - tStart) > 9)
+
 	sys.exit(0)
 	
 if __name__ == '__main__':
