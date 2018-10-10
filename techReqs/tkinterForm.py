d0cdd57710be32f3eded361e86acf0f37668e96c
Alexander <iampopovich@gmail.com>
2018-10-10 18:41:46 +0300

    upd 181010
    appended features:
    - u can create and append new lines for tech requirements
    - u can edit created lines ( now it's possible with appending new line only)
    - new design (wow)

--- techReqs/tkinterForm.py
+++ techReqs/tkinterForm.py
@@ -10,46 +10,69 @@ import standardLibrary
 class techReqsApp:
 	def __init__(self, master):
 		self.master = master
+		self.master.resizable(False, False)
 		self.title = master.title('Noname module')
 		self.frame_left = LabelFrame(master, text = 'макет выбора1')
-		self.frame_left.grid(row = 0, column = 0,sticky = 'wens')
+		self.frame_left.grid(row = 0, column = 0, sticky = 'wens')
+		self.frame_mid = LabelFrame(master, text = 'макет редактора')
+		self.frame_mid.grid(row = 0, column = 1, sticky = 'wens')
 		self.frame_right = LabelFrame(master, text = 'макет результата')
-		self.frame_right.grid(row = 0, column = 1,sticky = 'wens')
-		self.vsb_text = Scrollbar(self.frame_left, orient = VERTICAL)
-		self.vsb_text.grid(row = 0, column = 1,sticky = 'wens')
-		self.hsb_text = Scrollbar(self.frame_left, orient = HORIZONTAL)
-		self.hsb_text.grid(row = 1, column = 0,sticky = 'wens')
-		self.tree = ttk.Treeview(self.frame_left, yscrollcommand = self.vsb_text.set, xscrollcommand = self.hsb_text.set )
-		self.tree.grid(row = 0, column = 0)
-		self.hsb_text.config(command = self.tree.xview)
-		self.vsb_text.config(command = self.tree.yview)
-		self.right_textBox = Text(self.frame_right, font = 'arial 7',wrap = WORD)
-		self.right_textBox.grid(row = 0, column = 0, sticky = 'wens')
-		self.rigth_subFrame = Frame(self.frame_right)
-		self.rigth_subFrame.grid(row = 1, column = 0)
-		self.symbolsList = 
-		self.commitButton = Button(,text = 'сформировать текст ТТ')
+		self.frame_right.grid(row = 0, column = 2, sticky = 'wens')
+		self.vsb_tree = Scrollbar(self.frame_left, orient = VERTICAL)
+		self.vsb_tree.pack(side = RIGHT, fill = Y)
+		self.hsb_tree = Scrollbar(self.frame_left, orient = HORIZONTAL)
+		self.hsb_tree.pack(side = BOTTOM, fill = X)
+		self.tree = ttk.Treeview(self.frame_left, yscrollcommand = self.vsb_tree.set, xscrollcommand = self.hsb_tree.set)
+		self.tree.pack(side = LEFT, fill = BOTH)
+		self.hsb_tree.config(command = self.tree.xview)
+		self.vsb_tree.config(command = self.tree.yview)
 		
-		##tree.bind("<Double-1>", lambda x: OnDoubleClick(right_textBox,"<Double-1>"))
+		self.mid_textBox = Text(self.frame_mid, font = 'arial 7',wrap = WORD)
+		self.mid_textBox.grid(row = 0, column = 0, sticky = 'wens')
+		
+		self.appendButton = Button(self.frame_mid, text = 'добавить пункт ТТ', command = lambda: self.commitLine(self))
+		self.appendButton.grid(row = 1, column = 0, sticky = 'wens')
+		self.tree.bind("<Double-1>", lambda x: self.appendNewLine(self,"<Double-1>"))
+		
+		self.right_listBox = Listbox(self.frame_right, height = 18, width = 50)
+		self.right_listBox.grid(row = 0, column = 0, sticky = 'wens')
+		self.right_listBox.bind("<Double-1>", lambda x: self.editCurrentLine(self, "<Double-1>"))
+
+		self.commitButton = Button(self.frame_right, text = 'создать ТТ')
+		self.commitButton.grid(row = 1, column = 0, sticky = 'wens')
 		#self.button_frame = LabelFrame(self.frame_left, text = 'макет панели спецсимволов')
 		#self.button_frame.grid(row = 1, column = 0)
-		#self.specButton01 = Button(self.button_frame, height = 1, width = 3, command = lambda: addSymbol(self.right_textBox,'b1')).grid(row = 0, column = 0)
+		#self.specButton01 = Button(self.button_frame, height = 1, width = 3, command = lambda: addSymbol(self.mid_textBox,'b1')).grid(row = 0, column = 0)
 	
-	
+	#def onClickCommit(self,master):
+	#	self.right_listBox
+	#	self.right_listBox
+	#	pass
+
+	def commitLine(self,master):
+		text = self.mid_textBox.get("1.0",'end-1c')
+		self.right_listBox.insert(END,text)
+		self.mid_textBox.delete('1.0', END)
+
 	def fillTree(self, master):
-		self.tree["columns"]=("one","two")
 		for i in range(1,500):
 			self.tree.insert("", i, "dir%s"%i, text="Dir %s"%i)
 			self.tree.insert("dir%s"%i, i, text=" sub dir %s" %i,values=("%sA" %i," %sB" %i))
-		
 		
 	def addSymbol(text, keyval):
 		symbols = {'b1':'elem1', 
 					'b2':'elem2'}
 		text.insert(END, ' '+symbols[keyval])
 
-	def OnDoubleClick(text,event):
-		text.insert(END, 'good')
+	def appendNewLine(self, master,event):
+		item = self.tree.selection()[0]
+		self.mid_textBox.insert(END,("you clicked on", self.tree.item(item,"text")))
+
+	def editCurrentLine(self, master,event):
+		self.mid_textBox.delete('1.0', END)
+		index = self.right_listBox.curselection()[0]
+		value = self.right_listBox.get(index)
+		self.mid_textBox.insert(END,value)
 
 	def loadFont():
 		fontPath = '%s\\GOST_Type_A.ttf' %os.path.dirname(os.path.realpath(__file__))
