bc392ebf7e6d26c64d6a536fc6511eabc4559e33
Alexander <iampopovich@gmail.com>
2018-10-13 19:36:51 +0300

    upd 181013
    что работает : 
    - ресайз текста в канвасе
    - слайдер для ресайза текста
    - текст рендерится по значению слайдера
    - при редактировании запоминается пункт (изменить другие не удастся)
    - при удалении пунктов "пачкой" отрабатывается корректно
    - при пустом листе пунктов ТТ не вылетает при нажатии клавиши удаления
    - при изменении пунктов / состава пунктов - канвас обновляется
    - листенер больше не нужен, исправлена привязка к обновлению статуса кнопок
    что не работает :
    - многострочные пункты отрисовываются криво (нужен пересчет позиции текста)
    - не работает подгрузка кастомного шрифта ( замена канваса на PIL image или смена фрейма на Qt позже)
    - скроллбар для канваса имеет жесткую позицию на 1500 пх, нужен пересчет при добавлении и обновлении , либо смена ФВ
    - возможно придется сменить слайдер на выпадающий листбокс с размером шрифта
    - не работает выдержка красной строки (импрувать регэкспы)
    
    версия тестируется в ходе разработки. пока пофиксил все всплывающие бг

--- techReqs/tkinterForm.py
+++ techReqs/tkinterForm.py
@@ -18,7 +18,7 @@ class techReqsApp:
 		self.frame_mid.grid(row = 0, column = 1, sticky = 'wens')
 		self.frame_midSub = LabelFrame(self.frame_mid, text = 'макет списка')
 		self.frame_midSub.grid(row = 2, column = 0, sticky = 'wens', columnspan = 3)
-		self.frame_right = LabelFrame(parent, text = 'макет результата', bg= 'green')
+		self.frame_right = LabelFrame(parent, text = 'макет результата')
 		self.frame_right.grid(row = 0, column = 2, sticky = 'wens')
 		self.vsb_tree = Scrollbar(self.frame_left, orient = VERTICAL)
 		self.vsb_tree.pack(side = RIGHT, fill = Y)
@@ -28,17 +28,20 @@ class techReqsApp:
 		self.tree.pack(side = LEFT, fill = BOTH)
 		self.hsb_tree.config(command = self.tree.xview)
 		self.vsb_tree.config(command = self.tree.yview)
-		self.tree.bind("<Double-1>", lambda x: self.appendTemplate(self,"<Double-1>"))
+		self.tree.bind('<Double-1>', lambda x: self.appendTemplate(self,'<Double-1>'))
 		self.mid_textBox = Text(self.frame_mid, height = 10, font = 'arial 10',wrap = WORD)
 		self.mid_textBox.grid(row = 0, column = 0, sticky = 'wens', columnspan = 3)
 		self.mid_textBox.focus()
 		
-		self.appendButton = Button(self.frame_mid, text = 'добавить пункт ТТ', command = lambda: self.appendLine(self))
+		self.appendButton = Button(self.frame_mid, text = 'добавить пункт ТТ')
+		self.appendButton.config(command = lambda: self.appendLine(self))
 		self.appendButton.grid(row = 1, column = 0, sticky = 'wens')
-		self.commitEditedButton = Button(self.frame_mid, text = 'изменить пункт ТТ', command = lambda: self.commitEditedParagraph(self))
+		self.commitEditedButton = Button(self.frame_mid, text = 'изменить пункт ТТ')
+		self.commitEditedButton.config(command = lambda: self.commitEditedParagraph(self))
 		self.commitEditedButton.grid(row = 1, column = 1, sticky = 'wens')
 		self.commitEditedButton.config(state = DISABLED)
-		self.removeSelectedButton = Button(self.frame_mid, text = 'удалить пункт ТТ', command = lambda: self.removeCurrentSelection(self))
+		self.removeSelectedButton = Button(self.frame_mid, text = 'удалить пункт ТТ')
+		self.removeSelectedButton.config(command = lambda: self.removeCurrentSelection(self))
 		self.removeSelectedButton.grid(row = 1, column = 2, sticky = 'wens')
 		#self.removeSelectedButton.config(state=DISABLED) #see also listener definition at the bottom
 		self.removeSelectedButton.config(state = NORMAL)
@@ -55,7 +58,7 @@ class techReqsApp:
 		self.hsb_listBox.config(command = self.midSub_listBox.xview)
 		self.vsb_listBox.config(command = self.midSub_listBox.yview)
 
-		self.midSub_listBox.bind("<Double-1>", self.editCurrentLine)
+		self.midSub_listBox.bind('<Double-1>', self.editCurrentLine)
 		self.midSub_listBox.bind('<Button-1>', self.setCurrent)
 		self.midSub_listBox.bind('<B1-Motion>', self.shiftSelection)
 		self.commitButton = Button(self.frame_midSub, text = 'создать ТТ')
@@ -63,41 +66,41 @@ class techReqsApp:
 
 		self.vsb_resultCanvas = Scrollbar(self.frame_right, orient = VERTICAL)
 		self.vsb_resultCanvas.pack(side = RIGHT, fill = Y)
-		self.resultCanvas = Canvas(self.frame_right)
-		self.resultCanvas.config(yscrollcommand = self.vsb_resultCanvas.set)
+		self.resultCanvas = Canvas(self.frame_right,width = 700) #канвас оказался лютой заплаткой. мб заменю на пиловский image с ресайзом и кастомным фонтом
+		self.resultCanvas.config(yscrollcommand = self.vsb_resultCanvas.set)#, scrollregion=(0,0,0,1500))
 		self.resultCanvas.pack(side = TOP, expand = True, fill = BOTH)
+		self.resultCanvas.bind('<MouseWheel>', self.on_mousewheel)
 		self.vsb_resultCanvas.config(command = self.resultCanvas.yview)
-		self.slider_resultCanvas = Scale(self.frame_right, to = 100, orient = HORIZONTAL)
-		self.slider_resultCanvas.pack(side = BOTTOM, fill = X)
-		
-		#self.button_frame = LabelFrame(self.frame_left, text = 'макет панели спецсимволов')
-		#self.button_frame.grid(row = 1, column = 0)
-		#self.specButton01 = Button(self.button_frame, height = 1, width = 3, command = lambda: addSymbol(self.mid_textBox,'b1')).grid(row = 0, column = 0)
-	
-	#def onClickCommit(self,parent):
-	#	self.midSub_listBox
-	#	self.midSub_listBox
-	#	pass
+		self.scale_resultCanvas = Scale(self.frame_right, from_= 5, to = 20, orient = HORIZONTAL)
+		self.scale_resultCanvas.set(10) #юзлес фича, мб заменю на выпадающий листбокс, занимает много места и не выглядит юзабельно 
+		self.scale_resultCanvas.config(command = lambda x: self.resizeCanvasFont(self))
+		self.scale_resultCanvas.pack(side = BOTTOM, fill = X)
 
+	def on_mousewheel(self, event):
+		self.resultCanvas.yview_scroll(int(-1*(event.delta/120)), 'units')	# юзлес
+	
+	def resizeCanvasFont(self,master):
+		self.refreshResultCanvas(self)
+	
 	def setCurrent(self, event):
 		self.midSub_listBox.curIndex = self.midSub_listBox.nearest(event.y)
 
 	def shiftSelection(self, event):
-		i = self.midSub_listBox.nearest(event.y)
-		if i < self.midSub_listBox.curIndex:
-			x = self.midSub_listBox.get(i)
-			self.midSub_listBox.delete(i)
-			self.midSub_listBox.insert(i+1, x)
-			self.midSub_listBox.curIndex = i
-		elif i > self.midSub_listBox.curIndex:
-			x = self.midSub_listBox.get(i)
-			self.midSub_listBox.delete(i)
-			self.midSub_listBox.insert(i-1, x)
-			self.midSub_listBox.curIndex = i
+		index = self.midSub_listBox.nearest(event.y)
+		if index < self.midSub_listBox.curIndex:
+			x = self.midSub_listBox.get(index)
+			self.midSub_listBox.delete(index)
+			self.midSub_listBox.insert(index+1, x)
+			self.midSub_listBox.curIndex = index
+		elif index > self.midSub_listBox.curIndex:
+			x = self.midSub_listBox.get(index)
+			self.midSub_listBox.delete(index)
+			self.midSub_listBox.insert(index-1, x)
+			self.midSub_listBox.curIndex = index
 		self.recalculateParagraphs(self)
 
 	def appendLine(self,parent):
-		text = self.mid_textBox.get("1.0",'end-1c') #получаем содержимое поля ввода текста
+		text = self.mid_textBox.get('1.0','end-1c') #получаем содержимое поля ввода текста
 		try:
 			text = text.replace(re.match(r'[0-9]{1,3}\.[\s]{0,}|[,\.;\'~!@\#$%^&*()_+"]{1,}',text).group(0),'') #стрипаем введенный разрабом порядковый номер, если он есть
 		except: pass
@@ -106,12 +109,12 @@ class techReqsApp:
 		self.midSub_listBox.insert(END,text) #добавляем элемент в конец списка пунктов. мб можно будет вводить в произвольную позицию
 		self.mid_textBox.delete('1.0', END) #удаляем все содержимое поля воода
 		self.midSub_listBox.yview_scroll(lastIndex,'units') #переводит фокус на последний добавленный элемент 
-		self.refreshResult(self)
+		self.refreshResultCanvas(self)
 
 	def fillTree(self, parent):
 		for i in range(1,500):
-			self.tree.insert("", i, "dir%s"%i, text="Dir %s"%i)
-			self.tree.insert("dir%s"%i, i, text=" sub dir %s" %i, values =("%sA" %i," %sB" %i))
+			self.tree.insert('', i, 'dir%s'%i, text='Dir %s'%i)
+			self.tree.insert('dir%s'%i, i, text=' sub dir %s' %i, values = ('%sA' %i,' %sB' %i))
 
 	def addSymbol(text, keyval):
 		symbols = {'b1':'elem1', 
@@ -120,7 +123,7 @@ class techReqsApp:
 
 	def appendTemplate(self, parent,event):
 		item = self.tree.selection()[0]
-		self.mid_textBox.insert(END,("you clicked on", self.tree.item(item,"text")))
+		self.mid_textBox.insert(END,('you clicked on', self.tree.item(item,'text')))
 
 	def recalculateParagraphs(self, parent):
 		for index in range(self.midSub_listBox.size()):
@@ -132,6 +135,7 @@ class techReqsApp:
 			self.midSub_listBox.insert(index,text)
 
 	def editCurrentLine(self,event):
+		self.mid_textBox.focus()
 		self.midSub_listBox.config(state = DISABLED)
 		self.commitEditedButton.config(state = NORMAL)
 		self.appendButton.config(state = DISABLED)
@@ -144,9 +148,9 @@ class techReqsApp:
 		except:	pass
 		self.mid_textBox.insert(END,text)
 	
-	def commitEditedParagraph(self,mastrer):
+	def commitEditedParagraph(self, parent):
 		index = self.midSub_listBox.index(ACTIVE)#self.midSub_listBox.curselection()[0]
-		text = self.mid_textBox.get("1.0",'end-1c')
+		text = self.mid_textBox.get('1.0','end-1c')
 		try:
 			text = str(index+1)+'. ' + text.replace(re.match(r'[0-9]{1,3}\.[\s]{0,}|[,\.;\'~!@\#$%^&*()_+"]{1,}',text).group(0),'')
 		except: text = str(index+1)+'. ' + text
@@ -162,7 +166,7 @@ class techReqsApp:
 			self.commitEditedButton.config(state = NORMAL)
 			self.removeSelectedButton.config(state = DISABLED)
 			self.appendButton.config(state = DISABLED)
-		self.refreshResult(self)
+		self.refreshResultCanvas(self)
 
 	def removeCurrentSelection(self, parent): 
 		list_index = self.midSub_listBox.curselection()
@@ -172,14 +176,15 @@ class techReqsApp:
 			[self.midSub_listBox.insert(END, item) for item in list_temp]
 			self.recalculateParagraphs(self)
 		else: pass
-		self.refreshResult(self)
+		self.refreshResultCanvas(self)
 	
-	def refreshResult(self, parent):
+	def refreshResultCanvas(self, parent):
+		textfont = 'Arial %s' %(self.scale_resultCanvas.get())
 		self.resultCanvas.delete('all')
-		XBASE, YBASE, DISTANCE = 10, 20, 20
+		XBASE, YBASE, DISTANCE = 10, 20, int(re.search(r'[0-9]{1,}',textfont).group(0))+10
 		if self.midSub_listBox.size()!= 0:
 			for i,item in enumerate(self.midSub_listBox.get(0,END)):
-				self.resultCanvas.create_text((XBASE, YBASE + i * DISTANCE), anchor = W, fill='blue', text = item)
+				self.resultCanvas.create_text((XBASE, YBASE + i * DISTANCE), font = textfont, anchor = W, fill='blue', text = item)
 		pass
 
 	###experimental unworked feture
